f67773f9f1ed0eb9130e352c63e065150d2f65cb
Jack Amin <jack@cd.com.au>
2014-08-14 17:18:03 +1000

    Bootstrap framework with Codekit

--- README.md
+++
@@ -1,3 +0,0 @@
-<h2 id="cdt-website">cdt-website</h2>
-<p>===========</p>
-<h3 id="cdt-website-using-bootstrap-framework-this-is-for-the-testing-purpose-only-">cdt website using bootstrap framework (this is for the testing purpose only)</h3>
--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,1 @@
-function toggleGlyphicon(e) {
-	$(e.target)
-		.prev('.panel-heading')
-		.find("i.indicator")
-		.toggleClass('glyphicon-plus glyphicon-minus');
-}
-
-$('#accordion').on('hidden.bs.collapse', toggleGlyphicon);
-$('#accordion').on('shown.bs.collapse', toggleGlyphicon);
+/* Custom Javascript Starts here */
