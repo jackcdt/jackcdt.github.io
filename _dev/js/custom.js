f3c1793f3468ba5e178336a09d9adf7e2efb51c7
irenaSh <citydesktop@Lindas-iMac.local>
2014-08-19 09:46:18 +1000

    Landing page carousel - using LESS

--- _dev/js/custom.js
+++ _dev/js/custom.js
@@ -1,9 +1,24 @@
-function toggleGlyphicon(e) {
-	$(e.target)
-		.prev('.panel-heading')
-		.find("i.indicator")
-		.toggleClass('glyphicon-plus glyphicon-minus');
-}
+$(function(){
+	// Landing page courses home-accordion: manage "+"/"-"
+	function toggleGlyphicon(e) {
+		$(e.target)
+			.prev('.panel-heading')
+			.find("i.indicator")
+			.toggleClass('glyphicon-plus glyphicon-minus');
+	}
+	
+	$('#accordion').on('hidden.bs.collapse', toggleGlyphicon);
+	$('#accordion').on('shown.bs.collapse', toggleGlyphicon);
+	
+	// Landing page banner carousel - loop through all slides twice, then pause the carousel
+	function manageCarouselMovement() {
+		++manageCarouselMovement.count;
+		
+		if (manageCarouselMovement.count == 14){
+			$('#myCarousel').carousel('pause');
+		}
+	}
+	manageCarouselMovement.count = 0;
 
-$('#accordion').on('hidden.bs.collapse', toggleGlyphicon);
-$('#accordion').on('shown.bs.collapse', toggleGlyphicon);
+	$('#myCarousel').on('slid.bs.carousel', manageCarouselMovement);
+});
